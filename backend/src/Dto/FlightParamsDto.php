f5af80ae4b63de1b480ff77ea5e03f653768ca95
Eduardo Rodriguez <eduardo.rodriguezgalan@atexis.com>
2025-04-07 13:43:36 +0200

    dto

---
+++ backend/src/Dto/FlightParamsDto.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Dto;
+
+use InvalidArgumentException;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class FlightParamsDto
+{
+    public function __construct(
+        #[Assert\NotBlank]
+        #[Assert\Length(min: 3, max: 4)]
+        private string $airport,
+        #[Assert\Positive]
+        private int $startTime,
+        #[Assert\Positive]
+        private int $endTime
+    )
+    {
+        $sevenDays = 7 * 24 * 3600;
+
+        if ($endTime < $startTime){
+            throw new InvalidArgumentException("Start time must be less than end time");
+        }
+
+        if ($endTime-$startTime > $sevenDays){
+            throw new InvalidArgumentException("Start after end time or more than seven days of data requested");
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getStartTime(): int
+    {
+        return $this->startTime;
+    }
+
+    /**
+     * @param int $startTime
+     */
+    public function setStartTime(int $startTime): void
+    {
+        $this->startTime = $startTime;
+    }
+
+    /**
+     * @return int
+     */
+    public function getEndTime(): int
+    {
+        return $this->endTime;
+    }
+
+    /**
+     * @param int $endTime
+     */
+    public function setEndTime(int $endTime): void
+    {
+        $this->endTime = $endTime;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAirport(): string
+    {
+        return $this->airport;
+    }
+
+    /**
+     * @param string $airport
+     */
+    public function setAirport(string $airport): void
+    {
+        $this->airport = $airport;
+    }
+}
